35465f251465b45852fbefd09de6947cb5854bad
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-11-10 17:35:59 +0430

    modified this fille

--- modules/setTime.js
+++ modules/setTime.js
@@ -1,12 +1,9 @@
-import DateTime from './luxon.js';
-
-const startTime = () => {
-  const localTime = DateTime.local();
+/* eslint-disable no-undef */
+export default function startTime() {
+  const localTime = luxon.DateTime.local();
   const timeNow = document.querySelector('.time');
   timeNow.innerText = `${localTime.toLocaleString(
-    DateTime.DATE_FULL,
-  )}, ${localTime.toLocaleString(DateTime.TIME_WITH_SECONDS)}`;
+    luxon.DateTime.DATE_FULL,
+  )}, ${localTime.toLocaleString(luxon.DateTime.TIME_WITH_SECONDS)}`;
   setTimeout(startTime, 1000);
-};
-
-export default startTime;
+}
